82e4729b19ffc42ab054477fec5df683da6a7c76
Ralf Herbrich <rherbrich@gmail.com>
2024-01-01 15:30:30 +0100

    Added solution to Part II

--- 2023/day4/src/main.rs
+++ 2023/day4/src/main.rs
@@ -52,6 +52,56 @@ fn part1(filename: &str) {
     println!("sum: {}", sum);
 }
 
+// solution for the second part of the puzzle
+fn part2(filename: &str) {
+    // read a file named "input.txt" line by line
+    let reader = match File::open(filename) {
+        Ok(file) => BufReader::new(file),
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+    // read the file f line by line into input
+    let mut points = reader.lines()
+        .map(|l| {
+            let str = l.unwrap();
+            let cards = str
+                .split(':')
+                .collect::<Vec<&str>>()[1]
+                .split('|')
+                .collect::<Vec<&str>>();
+
+            let winning_cards = cards[0]
+                .split_ascii_whitespace()
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>();
+            
+            let no_matches = cards[1]
+                .split_ascii_whitespace()
+                .map(|x| { 
+                    if winning_cards.contains(&x.parse::<u32>().unwrap()) {
+                        1
+                    } else {
+                        0
+                    }
+                })
+                .sum::<u32>();
+
+            no_matches
+        })
+        .map(|x| (1, x))
+        .collect::<Vec<(u32,u32)>>();
+
+    for i in 0..points.len() {
+        for j in 0..points[i].1 {
+            points[i + 1 + (j as usize)].0 += points[i].0;
+        }
+    }
+
+    println!("sum: {:?}", points.iter().map(|(pts, _)| pts).sum::<u32>());
+}
+
+
 fn main() {
     part1("input.txt");
+    part2("input.txt")
 }
