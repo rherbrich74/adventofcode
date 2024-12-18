92d514da73a4d827d2ae534446746d838ecf9c87
Ralf Herbrich <rherbrich@gmail.com>
2024-12-18 07:27:30 +0100

    Partial solution for Part2 for Day 9

--- 2024/day9/solution.jl
+++ 2024/day9/solution.jl
@@ -3,6 +3,11 @@
 # 2024 by Ralf Herbrich
 # Hasso Plattner Institute, University of Potsdam, Germany
 
+struct FileBlock
+    file_id::Int
+    length::Int
+end
+
 # reads the input data from a file
 function read_input(filename)
     open(filename) do file
@@ -14,7 +19,7 @@ end
 function solution1(compressed)
     # decompresses the compressed string
     function decompress(compressed)
-        output = Array{String}(undef, 0)
+        output = Vector{String}()
         for file_id in 1:length(compressed)÷2
             # repeat string(file_id) compressed[2*i-1] many times
             for _ = 1:parse(Int, compressed[2*file_id-1])
@@ -34,41 +39,98 @@ function solution1(compressed)
 
     # pack the decompressed string
     function pack(decompressed)
-        packed = collect(decompressed)
+        packed = Vector{String}()
         i = 1
-        j = length(packed)
+        j = length(decompressed)
 
-        while i < j
-            if packed[i] == '.'
-                packed[i], packed[j] = packed[j], packed[i]
-
-                while packed[j] == '.' && i < j
+        while i <= j
+            if decompressed[i] != "."
+                # copy each file_id from the left
+                push!(packed, decompressed[i])
+                i += 1
+            else
+                # otherwise copy the file_id from the right
+                push!(packed, decompressed[j])
+                j -= 1
+                while decompressed[j] == "." && i <= j
                     j -= 1
                 end
+                i += 1
             end
-            i += 1
         end
-        println("i = ", i, " j = ", j)
 
-        return String(packed[1:i])
+        return packed
     end
 
     # computes the checksum
     function checksum(packed)
+        sum = 0
+        for (i, s) in enumerate(packed)
+            inc = (i -1) * parse(Int, s)
+            sum += inc
+        end
+        return sum
     end
 
-    decompressed = decompress(compressed)
-    println("Decompressed = ", length(decompress(compressed)))
-    # println("Packed = ", pack(decompress(compressed)))
+    return checksum(pack(decompress(compressed)))
 end
 
 # solves the second part of the puzzle
 function solution2(compressed)
+    # decompresses the compressed string
+    function decompress(compressed)
+        output = Vector{FileBlock}()
+        for file_id in 1:length(compressed)÷2
+            push!(output, FileBlock(file_id-1, parse(Int, compressed[2*file_id-1])))
+            push!(output, FileBlock(-1, parse(Int, compressed[2*file_id])))
+        end
+        push!(output, FileBlock(length(compressed)÷2, parse(Int, compressed[end])))
+        return output
+    end
+
+    # pack the decompressed file blocks
+    function pack(decompressed::Vector{FileBlock})
+        packed = Vector{FileBlock}()
+        i = 1
+        j = length(decompressed)
+
+        while i <= j
+            if decompressed[i].file_id != -1
+                # copy each file_id from the left
+                push!(packed, decompressed[i])
+                i += 1
+            else
+                if decompressed[j].length <= decompressed[i].length
+                    push!(packed, FileBlock(decompressed[j].file_id, decompressed[j].length))
+                    
+                    # otherwise copy the file_id from the right
+                    push!(packed, decompressed[j])
+                    j -= 1
+                    while decompressed[j].file_id == -1 && i <= j
+                        j -= 1
+                    end
+                    i += 1
+                else
+                    # merge the two blocks
+                    push!(packed, FileBlock(decompressed[i].file_id, decompressed[i].length + decompressed[j].length))
+                    j -= 1
+                    i += 1
+                end
+                # otherwise copy the file_id from the right
+                push!(packed, decompressed[j])
+                j -= 1
+                while decompressed[j] == "." && i <= j
+                    j -= 1
+                end
+                i += 1
+            end
+        end
+    end
+
+    println(decompress(compressed))
 end
 
 compressed = read_input("/Users/rherbrich/src/adventofcode/2024/day9/test.txt")
-
-# println("Compressed = ", compressed)
 
 println("Solution 1 = ", solution1(compressed))
 println("Solution 2 = ", solution2(compressed))
